821bb2138f508a3b0f28daf426cd9b75fe06448a
yangwoohyeon <dngus0717@gmail.com>
2024-10-12 19:53:06 +0900

    RequestParam 공부

--- src/main/java/com/example/demo12/controller/IndexController.java
+++ src/main/java/com/example/demo12/controller/IndexController.java
@@ -1,13 +1,19 @@
 package com.example.demo12.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/test")
-    public String test(){
+    @GetMapping(value = "test")//http://localhost:8080/test
+    public String test(@RequestParam("name") String name,
+            @RequestParam("name2") String name2,Model model){
+        model.addAttribute("username",name);
+        model.addAttribute("data","데이터");
+        model.addAttribute("name2",name2);
         return "test";
     }
 
