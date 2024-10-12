97ea69416f2cddd2cd40ae8254650947822a09d2
yangwoohyeon <dngus0717@gmail.com>
2024-10-12 21:13:00 +0900

    Thymeleaf 공부

--- src/main/java/com/example/demo12/controller/IndexController.java
+++ src/main/java/com/example/demo12/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.demo12.controller;
 
+import com.example.demo12.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +9,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
-    @GetMapping(value = "test")//http://localhost:8080/test
-    public String test(@RequestParam("name") String name,
-            @RequestParam("name2") String name2,Model model){
+    @GetMapping(value = "test")//bhttp://localhost:8080/test?name2=Doe
+    public String test(@RequestParam(value="name",required=false) String name,
+            @RequestParam(value="name2") String name2,Model model){
         model.addAttribute("username",name);
         model.addAttribute("data","데이터");
         model.addAttribute("name2",name2);
         return "test";
+    }
+    @GetMapping(value="/param6")
+    public String param6(ItemDto itemdto,Model model){
+        model.addAttribute("itemname",itemdto.getItemname());
+        model.addAttribute("itemprice",itemdto.getItemprice());
+        return "param6";
     }
 
 }
---
+++ src/main/java/com/example/demo12/dto/ItemDto.java
@@ -0,0 +1,12 @@
+package com.example.demo12.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ItemDto {
+    private String itemname;
+    private String itemprice;
+}
