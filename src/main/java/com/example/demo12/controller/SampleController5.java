2d589511f6c7685097cc98d6ccabf92901e6b40c
yangwoohyeon <dngus0717@gmail.com>
2024-10-15 23:33:18 +0900

    Thymeleaf 공부

--- src/main/java/com/example/demo12/controller/SampleController5.java
+++ src/main/java/com/example/demo12/controller/SampleController5.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class SampleController5 {
 
     @Autowired
-    private final MemberService memberService; // 중요
+    private final MemberService memberService;
 
     public SampleController5(MemberService memberService){this.memberService=memberService;}
     // SampleController5 클래스가 MemberService에 의존하고 있으며, 이를 안전하고 명확하게 주입받기 위해 필요하다.
