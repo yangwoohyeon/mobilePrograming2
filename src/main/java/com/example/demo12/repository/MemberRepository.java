cbe2b092a507f3cea8e1d2346d7f2cf393f4c9c3
yangwoohyeon <dngus0717@gmail.com>
2024-10-18 22:39:15 +0900

    JPA 공부

--- src/main/java/com/example/demo12/repository/MemberRepository.java
+++ src/main/java/com/example/demo12/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member,Long>{ //반환형 안붙힘!!
+public interface MemberRepository extends JpaRepository<Member,Long>{ //기본키가 Long형 이기 때문에 Long이 들어가는거다.
     List<Member> findAll(); //Spring Data JPA 장점 활용 public 붙히면 안됨
 
     Member findByEmail(String email);
