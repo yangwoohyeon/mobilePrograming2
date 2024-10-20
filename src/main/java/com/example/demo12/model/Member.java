71c86b1149305230b0b597dd0f76a8247511965d
yangwoohyeon <dngus0717@gmail.com>
2024-10-20 20:27:52 +0900

    JPA 공부

--- src/main/java/com/example/demo12/model/Member.java
+++ src/main/java/com/example/demo12/model/Member.java
@@ -13,12 +13,16 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY) //필드값을 DB가 자동 생성
     @Column(name = "NUM")
     private long num;
+
     @Column(nullable = false, length = 20)
     private String name;
+
     @Column(nullable = false, length = 20)
     private String id;
+
     @Column(nullable = false, length = 12)
     private String phone;
+
     @Column(nullable = false, unique = true) //unique=true ==> 고유해야 한다. 중복값 허용 X
     private String email;
 
