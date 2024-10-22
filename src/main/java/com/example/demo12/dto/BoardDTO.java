af6d8632c7de17d08b440f1fca38a9e9cf70a2e4
yangwoohyeon <dngus0717@gmail.com>
2024-10-22 19:52:17 +0900

    JPA 공부

--- src/main/java/com/example/demo12/dto/BoardDTO.java
+++ src/main/java/com/example/demo12/dto/BoardDTO.java
@@ -1,8 +1,14 @@
 package com.example.demo12.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class BoardDTO {
 
     private long num;
